ffcdfdfc9cd31ad189ffb33fc60e342d10011ba8
Jitender Kumar <111197506+Jitender72658@users.noreply.github.com>
2023-07-24 22:39:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const browserInfo = document.getElementById('info');
+const text = "You are using "+ navigator.userAgent;
+browserInfo.innerText = text;
+// const browserInfo = {
+//   userAgent: navigator.userAgent,
+//   appVersion: navigator.appVersion,
+//   platform: navigator.platform,
+// };
+
+console.log(browserInfo);
